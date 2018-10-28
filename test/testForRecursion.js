ebb2a9f5bbf2c33a21a28bc8ee80e36c9c22481a
montes5 <alexmontes5@yandex.ru>
2018-10-28 21:14:10 +0300

    refactor: refactor code

--- test/testForRecursion.js
+++ test/testForRecursion.js
@@ -1,31 +1,30 @@
 const assert = require('assert');
 const recursion = require('../recursion.js');
 
-const tree = {
-  value: 100,
-  left: {
-    value: 90,
-    left: {
-      value: 70,
-    },
-    right: {
-      value: 99,
-    },
-  },
-  right: {
-    value: 120,
-    left: {
-      value: 110,
-    },
-    right: {
-      value: 130,
-    },
-  },
-};
-
 describe('Test: recursion()', () => {
   it('1', () => {
-    const answ = recursion({"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}});
-    assert.deepEqual(answ, [[100], [90, 120], [70, 99, 110, 130]]);
+    const tree = {
+      value: 100,
+      left: {
+        value: 90,
+        left: {
+          value: 70,
+        },
+        right: {
+          value: 99,
+        },
+      },
+      right: {
+        value: 120,
+        left: {
+          value: 110,
+        },
+        right: {
+          value: 130,
+        },
+      },
+    };
+    const elem = recursion(tree);
+    assert.deepEqual(elem, [[100], [90, 120], [70, 99, 110, 130]]);
   });
 });
