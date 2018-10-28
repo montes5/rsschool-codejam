743561e4a492a937eec93a3c93858da29caf289f
montes5 <alexmontes5@yandex.ru>
2018-10-28 17:47:57 +0300

    make.js solution

---
+++ make.js
@@ -0,0 +1,7 @@
+function make(...arr) {
+  return function tempFunc(...args) {
+	if(typeof args[0] === "function") return arr.reduce(args[0]);
+  	arr = arr.concat(args);
+    return tempFunc;
+  }
+}
