32aceea3e80e0f4c7be2cd4c6ae0e8a522b27960
montes5 <alexmontes5@yandex.ru>
2018-10-28 20:59:34 +0300

    feat: prepare sumOfOther.js for test

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,4 +1,4 @@
-function sumOfOther(arr) {
+module.exports = function sumOfOther(arr) {
   let tempArr = [];
   for(let i = 0; i < arr.length; i++) {
     tempArr[i] = arr.slice(0,i).concat(arr.slice(i+1)).reduce((acc,elem) => acc + elem);
