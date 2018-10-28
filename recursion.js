1de5c45d886eed0cee65275cfd43f7b9f60b6c36
montes5 <alexmontes5@yandex.ru>
2018-10-28 17:51:18 +0300

    recursion.js solution

---
+++ recursion.js
@@ -0,0 +1,16 @@
+function recursion(obj) {
+  if(recursion.arr === undefined) recursion.arr = [];
+  let stage = 0;
+  let curVal = obj.value;
+  let tempTree = Object.assign({},tree);
+  while(curVal != tempTree.value) {
+    if(curVal > tempTree.value) tempTree = tempTree.right;
+    else tempTree = tempTree.left;
+    stage++;
+  }
+  if(!Array.isArray(recursion.arr[stage])) recursion.arr[stage] = [];
+  recursion.arr[stage].push(obj.value);
+  if(obj.left !== undefined) recursion(obj.left);
+  if(obj.right !== undefined) recursion(obj.right);
+  return recursion.arr;
+}
